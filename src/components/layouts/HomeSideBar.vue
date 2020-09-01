02216b69d88d760c8fc39328c81636d3a67d9ce4
acatzk <joshuaimalay@gmail.com>
2020-09-01 12:34:52 +0800

    Added Navigation Drawer and Display Links

---
+++ src/components/layouts/HomeSideBar.vue
@@ -0,0 +1,68 @@
+<template>
+    <v-navigation-drawer
+      v-model="show"
+      app
+      temporary
+      dark
+      src="@/assets/img/bgDrawer.jpg"
+    >
+      <v-list>
+        <v-list-item>
+          <v-list-item-avatar>
+            <img src="@/assets/logo.png" alt="Logo" />
+          </v-list-item-avatar>
+          <v-list-item-content>
+            <v-list-item-title class="title">Airclass Solution</v-list-item-title>
+            <v-list-item-subtitle>Official website</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+
+      <v-divider />
+
+      <v-list>
+        <v-list-item
+          v-for="([icon, text, link], i) in items"
+          :key="i"
+          link
+          @click="$vuetify.goTo(link)"
+        >
+          <v-list-item-icon class="justify-center">
+            <v-icon>{{ icon }}</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title class="subtitile-1">{{
+              text
+            }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item>
+            <v-btn rounded outlined text block large>
+                <v-icon medium left>mdi-account-circle</v-icon>
+                <span class="mr-2">Learn More</span>
+            </v-btn>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+</template>
+
+<script>
+    export default {
+        name: 'home-side-bar',
+        
+        props: ['items', 'visible'],
+
+        computed: {
+            show: {
+                get (){
+                    return this.visible
+                },
+                set (value) {
+                    if (!value) {
+                        this.$emit('close')
+                    }
+                }
+            }
+        }
+    }
+</script>
