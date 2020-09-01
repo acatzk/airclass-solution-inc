4ae20f4301dc9f211371bb62750b949abaa2446c
acatzk <joshuaimalay@gmail.com>
2020-09-01 13:35:11 +0800

    Import and used VueYoutubeEmeded

--- src/main.js
+++ src/main.js
@@ -3,10 +3,14 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
+import VueYouTubeEmbed from 'vue-youtube-embed'
+
 import "roboto-fontface/css/roboto/roboto-fontface.css";
 import "@mdi/font/css/materialdesignicons.css";
 
 Vue.config.productionTip = false;
+
+Vue.use(VueYouTubeEmbed)
 
 new Vue({
   router,
