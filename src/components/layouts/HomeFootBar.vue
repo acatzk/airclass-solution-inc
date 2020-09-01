5141ab75fbd88b0cf373457b801bbf8807d7c2a7
acatzk <joshuaimalay@gmail.com>
2020-09-02 07:55:41 +0800

    Added temporary HomeFooter Design

---
+++ src/components/layouts/HomeFootBar.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-footer dark padless>
+    <v-card flat tile class="secondary white--text text-center">
+      <v-card-text>
+        <v-btn
+          v-for="(icon, i) in icons"
+          :key="i"
+          class="mx-4 white--text"
+          :href="icon.link"
+          target="_blank"
+          icon
+        >
+          <v-icon size="24px">{{ icon.text }}</v-icon>
+        </v-btn>
+      </v-card-text>
+
+      <v-card-text class="white--text pt-0">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, a iste fugiat iusto dolore animi qui earum libero magnam officiis?
+      </v-card-text>
+
+      <v-divider></v-divider>
+
+      <v-card-text class="white--text">
+        {{ new Date().getFullYear() }} — <strong>Airclass Solution, Inc.</strong>
+      </v-card-text>
+    </v-card>
+  </v-footer>
+</template>
+
+<style scoped>
+.v-card {
+  width: 100%;
+}
+</style>
+
+<script>
+export default {
+  data: () => ({
+    icons: [
+      {
+        text: "mdi-facebook",
+        link: "",
+      },
+      {
+        text: "mdi-twitter",
+        link: "",
+      },
+      {
+        text: "mdi-linkedin",
+        link: "",
+      },
+      {
+        text: "mdi-instagram",
+        link: "",
+      },
+    ],
+  }),
+};
+</script>
