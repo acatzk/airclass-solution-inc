25cdd4a331eb61784f167da2680373c77fcd6745
acatzk <joshuaimalay@gmail.com>
2020-09-04 10:32:28 +0800

    Changed the svg image and add 3 categories of comport home

---
+++ src/components/pages/ComfortSection.vue
@@ -0,0 +1,38 @@
+<template>
+  <section id="comport">
+    <v-container fluid>
+      <v-row align="center" justify="center">
+        <v-col cols="10">
+          <v-row align="center" justify="center">
+            <v-col cols="12" md="6">
+              <v-container>
+                <h2 class="display-1 font-weight-medium pb-6" style="color: #313131;">Learn with the best from the comfort of your home</h2>
+                <v-row>
+                    <v-col cols="12">
+                        <v-icon class="pb-2" sm="6" large color="blue darken-1">mdi-cube-outline</v-icon><br>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae fugiat quae.
+                    </v-col>
+                     <v-col cols="12" sm="6">
+                        <v-icon class="pb-2" large color="blue darken-1">mdi-cast</v-icon><br>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae fugiat quae.
+                    </v-col>
+                     <v-col cols="12" sm="6">
+                        <v-icon class="pb-2" large color="blue darken-1">mdi-cellphone-link</v-icon><br>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae fugiat quae.
+                    </v-col>
+                </v-row>
+              </v-container>
+            </v-col>
+            <v-col cols="12" md="6" class="d-none d-md-flex mt-10">
+              <v-img
+                src="@/assets/img/comfort.svg"
+                class="d-block ml-auto mr-auto"
+                max-width="450px"
+              />
+            </v-col>
+          </v-row>
+        </v-col>
+      </v-row>
+    </v-container>
+  </section>
+</template>
