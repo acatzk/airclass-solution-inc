7c6891fd54e75d59940f69fdba24f645e6ad9558
acatzk <joshuaimalay@gmail.com>
2020-09-04 12:16:37 +0800

    Changed the image inside the assets img folder

--- src/components/pages/TestimonySection.vue
+++ src/components/pages/TestimonySection.vue
@@ -14,16 +14,16 @@
       return {
         items: [
           {
-            src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
+            src: require('@/assets/img/bgDownload.jpg'),
           },
           {
-            src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
+            src: require('@/assets/img/bgDrawer.jpg'),
           },
           {
-            src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg',
+            src: require('@/assets/img/bgHero.jpg'),
           },
           {
-            src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
+            src: require('@/assets/img/bghero1.jpg'),
           },
         ],
       }
