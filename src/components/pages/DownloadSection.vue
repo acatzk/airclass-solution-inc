9f6a53c43ec9f3f299d301ba09297e953125024e
acatzk <joshuaimalay@gmail.com>
2020-09-04 09:02:35 +0800

    Changed the title and added new description author and year published

--- src/components/pages/DownloadSection.vue
+++ src/components/pages/DownloadSection.vue
@@ -16,7 +16,7 @@
         </v-layout>
 
         <v-container>
-            <horizontal-list :items="videos" class="mx-12">
+            <horizontal-list :items="pdfs" class="mx-12">
                 <template v-slot:default="{ item }">
                     <v-hover v-slot="{ hover }">
                         <v-card
@@ -25,6 +25,7 @@
                             :class="{ up: hover }"
                             :elevation="hover ? 7 : 0"
                             class="mx-auto"
+                            max-width="400"
                         >
                             <v-img
                                 height="200px"
@@ -32,13 +33,18 @@
                                 :lazy-src="item.image"
                             >
                             </v-img>
-                            <v-card-subtitle class="pb-0">Number 10</v-card-subtitle>
-
-                            <v-card-text class="text--primary">
-                            <div>Whitehaven Beach</div>
-
-                            <div>Whitsunday Island, Whitsunday Islands</div>
+                            <v-card-text class="px-3">
+                                <div class="text--primary font-weight-medium">
+                                    {{ item.title }}
+                                </div>
+                                <div class="text-secondary pt-2">
+                                    {{ item.description }}
+                                </div>
+                                <div class="text-secondary pt-2 ">
+                                    <i class="font-weight-light">By {{ item.author }} ({{ item.publish_year }})</i>
+                                </div>
                             </v-card-text>
+
                             <v-card-actions>
                                 <v-spacer></v-spacer>
                                 <v-btn 
@@ -69,36 +75,54 @@
 
         data () {
             return {
-                videos: [
+                pdfs: [
                     {
                         id: 0,
-                        src: 'https://www.youtube.com/watch?v=mdu5lLpMH_w',
-                        image: 'https://image.freepik.com/free-vector/happy-kids-illustration_23-2147531838.jpg'
+                        title: "The Tale of Peter Rabbit",
+                        description: "Beatrix Potter's first and one of her best.",
+                        author: 'Beatrix Potter',
+                        publish_year: '2003',
+                        image: 'https://www.bigw.com.au/medias/sys_master/images/images/h15/h7a/11727109062686.jpg'
                     },
                     {
                         id: 1,
-                        src: 'https://www.youtube.com/watch?v=fYP-3q22too',
-                        image: 'https://image.freepik.com/free-vector/young-teacher-with-joyful-kids_74855-6162.jpg'
+                        title: 'Harold and the Purple Crayon',
+                        description: 'Imagination reigns in timeless classic about drawing boy.',
+                        author: 'Crockett Johnson',
+                        publish_year: '2012',
+                        image: 'https://www.scholastic.com/content5/media/products/34/9780439132534_mres.jpg'
                     },
                     {
                         id: 2,
-                        src: 'https://www.youtube.com/watch?v=A8hjmsBKM5w',
-                        image: 'https://image.freepik.com/free-vector/children-education_29937-3077.jpg'  
+                        title: 'Goodnight Moon',
+                        description: 'Ultimate going-to-bed book has stood the test of time.',
+                        author: 'Margaret Wise Brown',
+                        publish_year: '2004',
+                        image: 'https://prodimage.images-bn.com/pimages/9780694003617_p0_v2_s1200x630.jpg'  
                     },
                     {
                         id: 3,
-                        src: 'https://www.youtube.com/watch?v=A8hjmsBKM5w',
-                        image: 'https://image.freepik.com/free-vector/hikers-walk-bridge-crossing-river-forest_107791-2695.jpg'  
+                        title: ' The Snowy Day',
+                        description: "Classic captures a kid's delight in freshly fallen snow.",
+                        author: 'Ezra Jack Keats ',
+                        publish_year: '2003',
+                        image: 'https://images-na.ssl-images-amazon.com/images/I/81DDpwd-T4L._RI_.jpg'  
                     },
                     {
                         id: 4,
-                        src: 'https://www.youtube.com/watch?v=A8hjmsBKM5w',
-                        image: 'https://image.freepik.com/free-vector/new-normal-summer-camps_23-2148589072.jpg'  
+                        title: 'The Very Hungry Caterpillar',
+                        description: "Carle brings humor to caterpillar's development.",
+                        author: 'Eric Carle',
+                        publish_year: '2003',
+                        image: 'https://target.scene7.com/is/image/Target/GUEST_ea6b5786-9f05-471b-8c0d-440e863a97fd?wid=488&hei=488&fmt=pjpeg'  
                     },
                     {
                         id: 5,
-                        src: 'https://www.youtube.com/watch?v=A8hjmsBKM5w',
-                        image: 'https://image.freepik.com/free-vector/new-normal-summer-camps_23-2148592701.jpg'  
+                        title: 'Where the Wild Things Are',
+                        description: "Classic all-ages masterpiece wild imagination.",
+                        author: 'Maurice Sendak',
+                        publish_year: '2001',
+                        image: 'https://images-na.ssl-images-amazon.com/images/I/61ulN35aErL.jpg'  
                     }
                 ]
             }
