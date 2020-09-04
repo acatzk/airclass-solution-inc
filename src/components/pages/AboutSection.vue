9b7388162f7496890c170f63c7a2431b919411ae
acatzk <joshuaimalay@gmail.com>
2020-09-04 10:03:32 +0800

    About Page residesign section

--- src/components/pages/AboutSection.vue
+++ src/components/pages/AboutSection.vue
@@ -4,32 +4,29 @@
       <v-row align="center" justify="center">
         <v-col cols="10">
           <v-row align="center" justify="center">
-            <v-col cols="12" md="7">
-              <!-- <h1 class="font-weight-light display-2">👋</h1> -->
-              <h1 class="font-weight-light display-1 mb-3">
-                About Our Company 👋
-              </h1> 
-              <v-row>
-                <v-col 
-                    cols="12" 
-                    class="d-flex align-center"
-                    v-for="(detail, index) in details" :key="index"
-                >
-                  <v-img
-                    :src="detail.src"
-                    max-width="60px"
-                    class="mr-4"
-                  />
-                  <p class="text-justify">{{ detail.description }}</p>
-                </v-col>
-              </v-row>
-            </v-col>
-            <v-col cols="12" md="5" class="d-none d-md-flex">
+            <v-col cols="12" md="6" class="d-none d-md-flex mt-10">
               <v-img
                 src="@/assets/img/ill1.svg"
                 class="d-block ml-auto mr-auto"
-                max-width="400px"
+                max-width="450px"
               />
+            </v-col>
+            <v-col cols="12" md="6">
+              <v-container>
+                <p class="text-uppercase font-weight-bold blue--text darken-1">About Us</p>
+                <h2 class="display-2 font-weight-bold pb-6" style="color: #313131;">We scratch, build and play together</h2>
+                <p class="mt-2 grey--text line text-justify" style="line-height: 25px;">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor vero ad voluptatum iusto inventore ipsam quidem libero unde officiis! Magnam asperiores magni earum perspiciatis consequatur nobis ad veniam amet sequi!
+                </p>
+                <v-btn 
+                  outlined 
+                  color="deep-purple"
+                  class="mt-5 text-capitalize font-weight-medium"
+                  large
+                >
+                  Contact Us
+                </v-btn>
+              </v-container>
             </v-col>
           </v-row>
         </v-col>
@@ -69,6 +66,5 @@
 <style scoped>
 #about {
   background-color: #f4f7f5;
-  min-height: 100vh;
 }
 </style>
