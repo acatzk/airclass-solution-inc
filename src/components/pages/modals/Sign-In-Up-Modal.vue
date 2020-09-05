93cdab8601ad6f2fef2ebf053110b3d6e7e40e8b
acatzk <joshuaimalay@gmail.com>
2020-09-05 08:48:29 +0800

    Removed SignIn and Changed to Sing-In-Up vue component

---
+++ src/components/pages/modals/Sign-In-Up-Modal.vue
@@ -0,0 +1,67 @@
+<template>
+  <v-dialog v-model="show" transition="dialog-bottom-transition">
+    <v-card flat>
+      <v-container>
+        <v-toolbar flat>
+          <v-spacer></v-spacer>
+          <v-btn 
+            icon 
+            dark 
+            @click="show = !show"
+            color="black"
+          >
+            <v-icon>mdi-close</v-icon>
+          </v-btn>
+        </v-toolbar>
+        <v-row align="center" justify="center">
+          <v-col cols="10">
+            <v-row justify="center">
+              <v-col cols="12" sm="6">
+                sign in
+              </v-col>
+              <v-col cols="12" sm="6">
+                sign up
+              </v-col>
+            </v-row>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+
+  export default {
+    name: 'sign-in-up-modal',
+
+    props: ['visible'],
+
+
+    data () {
+        return {
+
+        }
+    },
+
+    computed: {
+      show: {
+        get () {
+          return this.visible
+        },
+        set (value) {
+          if (!value) {
+            this.$emit('close')
+          }
+        }
+      }
+    }
+    
+  }
+</script>
+
+<style scoped>
+  #contact {
+      background-color: #f4f7f5;
+  }
+</style>
