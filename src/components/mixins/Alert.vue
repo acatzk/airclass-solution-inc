509e6695006170d19a597eb50e9dc53a87beaef1
acatzk <joshuaimalay@gmail.com>
2020-09-05 13:11:43 +0800

    Displayed Alert error message

---
+++ src/components/mixins/Alert.vue
@@ -0,0 +1,18 @@
+<template>
+    <v-alert type="error">
+        {{ message }}
+    </v-alert>
+</template>
+
+<script>
+    export default {
+        name: 'alert',
+        
+        props: {
+            message: {
+                type: String,
+                required: true
+            }
+        }
+    }
+</script>
