5e250e669e6c38b2e51cbaea9d4efdba81729b3c
acatzk <joshuaimalay@gmail.com>
2020-09-05 11:16:32 +0800

    Import Sign In / Up modal

--- src/components/pages/HomeSection.vue
+++ src/components/pages/HomeSection.vue
@@ -3,44 +3,70 @@
 
       <v-layout class="hero-container">
         <v-row align="center" justify="center">
-          <v-col cols="12" md="7" sm="12">
-              <h1 class="display-2 font-weight-black pb-6" style="color: #313131;">Airclass Solution, Inc. </h1>
-              <p class="grey--text">
-                Learning English Language Through Video Calls.
-                Putting Children First. Preparing children for success in life
-              </p>
-              <v-btn 
-                x-large 
-                class="mt-3 text-capitalize pa-7" 
-                rounded 
-                color="primary" 
-                depressed
-              >
-                Sign Up for free
-              </v-btn>
-              <v-btn 
-                fab 
-                @click="$vuetify.goTo('#services')"
-                outlined
-                text
-                large
-                class="mt-3 ml-5"
-              >
-                <v-icon>mdi-arrow-down</v-icon>
-              </v-btn>
-          </v-col>
-          <v-col cols="12" md="5" class="d-none d-md-flex">
-            <img 
-              src="@/assets/img/herosvg.svg" 
-              alt="hero" 
-              class="side-hero-img d-block ml-auto mr-auto"
-            >
+          <v-col cols="11">
+            <v-row align="center" justify="center">
+              <v-col cols="12" md="7" sm="12">
+                <h1 class="display-2 font-weight-black pb-6" style="color: #313131;">Airclass Solution, Inc. </h1>
+                <p class="grey--text">
+                  Learning English Language Through Video Calls.<br/>
+                  Putting Children First. Preparing children for success in life
+                </p>
+                <v-btn 
+                  x-large 
+                  class="mt-3 text-capitalize pa-7" 
+                  rounded 
+                  color="primary" 
+                  depressed
+                  @click="authDialog = true"
+                >
+                  Sign Up for free
+                </v-btn>
+                <v-btn 
+                  fab 
+                  @click="$vuetify.goTo('#services')"
+                  outlined
+                  text
+                  large
+                  class="mt-3 ml-5"
+                >
+                  <v-icon>mdi-arrow-down</v-icon>
+                </v-btn>
+              </v-col>
+              <v-col cols="12" md="5" class="d-none d-md-flex">
+                <img 
+                  src="@/assets/img/herosvg.svg" 
+                  alt="hero" 
+                  class="side-hero-img d-block ml-auto mr-auto"
+                >
+              </v-col>
+            </v-row>
           </v-col>
         </v-row>
       </v-layout>
 
+      <sign-in-up-modal
+        :visible="authDialog"
+        @close="authDialog = false"
+      />  <!-- SIGN IN / SING UP AUTH MODAL -->
+
   </section>
 </template>
+
+<script>
+  export default {
+    name: 'home-section',
+
+    data () {
+      return {
+        authDialog: false
+      }
+    },
+    
+    components: {
+      SignInUpModal: () => import('@/components/pages/modals/Sign-In-Up-Modal')
+    }
+  }
+</script>
 
 <style scoped>
 .hero-container {
