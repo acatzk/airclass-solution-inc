e820670fec5970d4d9f2f5cf3e36269fdcfeca9b
acatzk <joshuaimalay@gmail.com>
2020-09-05 13:20:03 +0800

    Conditioned the firebase auth onAuthStateChanged token refresh

--- src/main.js
+++ src/main.js
@@ -3,16 +3,22 @@ import App from "./App.vue"
 import router from "./router"
 import store from "./store"
 import vuetify from "./plugins/vuetify"
-import { apolloProvider } from '@/services' 
+import { auth, apolloProvider } from '@/services' 
 
 import "@mdi/font/css/materialdesignicons.css"
 
 Vue.config.productionTip = false
 
-new Vue({
-  router,
-  store,
-  vuetify,
-  apolloProvider,
-  render: h => h(App)
-}).$mount("#app")
+let app = ''
+
+auth.onAuthStateChanged(() => {
+  if (!app) {
+    new Vue({
+      router,
+      store,
+      vuetify,
+      apolloProvider,
+      render: h => h(App)
+    }).$mount("#app")
+  }
+})
