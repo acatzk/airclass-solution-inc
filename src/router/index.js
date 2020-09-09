22cd5b442c43771ef890c2ff780c75631c98829d
acatzk <joshuaimalay@gmail.com>
2020-09-09 11:47:34 +0800

    Import auth in firebase and protected the dashboard during injection

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import { auth } from '@/services'
 
 Vue.use(VueRouter);
 
@@ -8,12 +9,39 @@ const routes = [
     path: '/',
     name: 'welcome',
     component: () => import('@/views/Welcome')
+  },
+  {
+    path: '/v',
+    name: 'home',
+    component: () => import('@/views/home/Home'),
+    meta: { requiresAuth: true },
+    children: [
+      {
+        path: 'dashboard',
+        name: 'dashboard',
+        component: () => import('@/views/home/Dashboard')
+      }
+    ]
   }
 ]
 
 const router = new VueRouter({
   routes,
   mode: 'history'
+})
+
+
+router.beforeEach((to, from, next) => {
+  const requiresAuth = to.matched.some(record => record.meta.requiresAuth)
+  const currentUser = auth.currentUser
+
+  if (requiresAuth && !currentUser) {
+    next('/')
+  } else if (!requiresAuth && currentUser) {
+    next('/v/dashboard')
+  } else {
+    next()
+  }
 })
 
 export default router
