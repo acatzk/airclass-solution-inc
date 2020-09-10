08ab3c9659d3bea194f7261796708fbd82a28a52
acatzk <joshuaimalay@gmail.com>
2020-09-10 15:42:45 +0800

    Added dummy button logout functionality

--- src/components/layouts/HomeSideBar.vue
+++ src/components/layouts/HomeSideBar.vue
@@ -1,5 +1,33 @@
 <template>
-    <div>
+    <div class="text-center">
         <h1>Navigation</h1>
+        <v-btn
+            x-large
+            color="yellow"
+            dark
+            @click="onClickLogout"
+        >
+            Log out here idiot
+        </v-btn>
     </div>
 </template>
+
+<script>
+
+    import { auth } from '@/services'
+
+    import { toastAlertStatus } from '@/utils'
+
+    export default {
+        name: 'home-side-bar',
+        
+        methods: {
+            onClickLogout () {
+                auth
+                .signOut()
+                .then(() => this.$router.push({ name: 'welcome' }))
+                .catch(error => toastAlertStatus(error, 'error'))
+            }
+        }
+    }
+</script>
