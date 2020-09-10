e9edff771f01245fe4c3d43c830adb1e12c92531
acatzk <joshuaimalay@gmail.com>
2020-09-10 10:08:55 +0800

    Import HomeSideBar vue component

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,5 +1,19 @@
 <template>
-    <div>
-        Home
+
+    <div class="home">
+        
+        <home-side-bar /> <!-- HOME SIDE BAR COMPONENT -->
+
     </div>
+
 </template>
+
+<script>
+    export default {
+        name: 'home',
+        
+        components: {
+            HomeSideBar: () => import('@/components/layouts/HomeSideBar')
+        }
+    }
+</script>
