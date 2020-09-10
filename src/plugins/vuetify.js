f134096ee5f6120ee4eed81f46d007fc0d7303b8
acatzk <joshuaimalay@gmail.com>
2020-09-10 10:19:35 +0800

    Registered standard colors

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -14,7 +14,21 @@ export default new Vuetify({
         error: "#FF5252",
         info: "#2196F3",
         success: "#4CAF50",
-        warning: "#FFC107"
+        warning: "#FFC107",
+        lightblue: "#14c6FF",
+        yellow: "#FFCF00",
+        pink: "#FF1976",
+        orange: "#FF8657",
+        magenta: "#C33AFC",
+        darkblue: "#1E2D56",
+        gray: "#909090",
+        neutralgray: "#9BA6C1",
+        green: "#2ED47A",
+        red: "#FF5c4E",
+        darkblueshade: "#308DC2",
+        lightgray: "#BDBDBD",
+        lightpink: "#FFCFE3",
+        white: "#FFFFFF"
       }
     }
   }
