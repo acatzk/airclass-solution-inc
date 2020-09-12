c69fbe88a90b93e1983176a664fe3d6682890d0e
acatzk <joshuaimalay@gmail.com>
2020-09-12 12:22:36 +0800

    Added router to action scroll indicator

--- src/router/index.js
+++ src/router/index.js
@@ -64,4 +64,20 @@ router.beforeEach((to, from, next) => {
   }
 })
 
+
+router.beforeResolve((to, from, next) => {
+  // If this isn't an initial page load.
+  NProgress.configure({ easing: 'ease', speed: 2000, showSpinner: false })
+  if (to.name) {
+    NProgress.start()
+  }
+  next()
+})
+
+router.afterEach((to, from) => {
+  // Complete the animation of the route progress bar.
+  NProgress.done()
+  window.scrollTo(0, 0)
+})
+
 export default router
