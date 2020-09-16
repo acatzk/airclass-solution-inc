33fb6a4c5add8eec6ba06bcf3644f853a49a6ef4
acatzk <joshuaimalay@gmail.com>
2020-09-16 21:12:54 +0800

    Added temporary profile pic design

--- src/components/pages/home-sections/ProfileSection.vue
+++ src/components/pages/home-sections/ProfileSection.vue
@@ -1,5 +1,48 @@
 <template>
-    <div>
-        <h1>This is Profile Section</h1>
-    </div>
+     <section id="profile-section">
+        <div class="cover-photo"></div>
+        <v-container fluid>
+            <v-row align="center" justify="center">
+                <v-col cols="10">
+                    <v-row align="center" justify="center">
+                        <v-col cols="12">
+                            <v-badge
+                                bordered
+                                color="success"
+                                dot
+                                offset-x="46"
+                                offset-y="20"
+                            >
+                                <v-avatar 
+                                    size="150" 
+                                    class="avatar-profile"
+                                >
+                                
+                                    <v-img
+                                        src="https://pbs.twimg.com/profile_images/1031140303791579136/TU9GctxI_400x400.jpg"
+                                        lazy-src="https://pbs.twimg.com/profile_images/1031140303791579136/TU9GctxI_400x400.jpg"
+                                    >
+                                    </v-img>
+                                </v-avatar>
+                            </v-badge>
+                        </v-col>
+                    </v-row>
+                </v-col>
+            </v-row>
+        </v-container>
+     </section>
 </template>
+
+<style scoped>
+.cover-photo {
+    width: 100%;
+    min-height: 30vh;
+    background: #14C6FF;
+}
+.avatar-profile {
+    position: relative;
+    bottom: 110px;
+    right: 20px;
+    border: 4px solid #fff;
+}
+</style>
