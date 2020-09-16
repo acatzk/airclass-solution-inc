fb3d1a7c3814bcdfec748682a7650ad1389ad2d6
acatzk <joshuaimalay@gmail.com>
2020-09-16 20:54:50 +0800

    Added firebase_id to fetch in data

--- src/graphql/queries/students.js
+++ src/graphql/queries/students.js
@@ -4,6 +4,7 @@ export const GET_STUDENT_QUERY = gql`
     query studentFullnameQuery($firebase_id: String!) {
         students(where: {firebase_id: {_eq: $firebase_id}}) {
             id
+            firebase_id
             firstname
             lastname
             profile_url
--- src/graphql/subscriptions/students.js
+++ src/graphql/subscriptions/students.js
@@ -4,6 +4,7 @@ export const GET_STUDENT_SUBSCRIPTION = gql`
     subscription studentFullnameSubscription($firebase_id: String!) {
         students(where: {firebase_id: {_eq: $firebase_id}}) {
             id
+            firebase_id
             firstname
             lastname
             profile_url
